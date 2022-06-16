a4c47d84a4cd7ad51d09b8c35da3c6e37b5eab7b
SamArif <samarif085@gmail.com>
2022-06-16 20:12:55 +0700

    Update main.blade.php

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -12,8 +12,6 @@
     <meta name="generator" content="Hugo 0.84.0">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Dashboard Template · Bootstrap v5.0</title>
-
-
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
